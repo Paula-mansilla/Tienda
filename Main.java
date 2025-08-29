db53d9161e6aff9ccbb1367e8f774cc83ce3b4bb
Paula Mansilla <pimansilla@scl.edu.gt>
2025-08-29 07:39:36 -0600

    main

--- Main.java
+++ Main.java
@@ -5,29 +5,20 @@ import views.VistaTienda;
 public class Main {
 public static void main(String[] args) {
 
-    String saludo = "Hola Mundo";
-    System.out.println(saludo.charAt(0));
-
-    for (int i = 0; i < saludo.length(); i++) {
-        System.out.println(saludo.charAt(i));
-    }
- 
     try {
         VistaTienda vistaTienda = new VistaTienda();
 
         //consulta
-        String urlApi = "https://jsonplaceholder.typicode.com/posts";
+        String urlApi = "https://jsonplaceholder.typicode.com/posts/99";
         String respuesta = TiendaFercher.obtenerProductos(urlApi);
         System.out.println("Respuest de la Api");
 
-        //Dashboard dashboard = new Dashboard(respuesta);
+        
 
         for (int i = 0; i < respuesta.length(); i++) {
-            String letra ="" + respuesta.charAt(i);
 
-            if (letra.equalsIgnoreCase("}")) {
-                System.out.println(respuesta.charAt(i));
-            }
+                System.out.print(respuesta.charAt(i));
+            
         }
 
 
--- control/TiendaFercher.java
+++ control/TiendaFercher.java
@@ -1,29 +1,52 @@
 package control;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class TiendaFercher {
+    
     public static String obtenerProductos(String urlApi){
+
         StringBuilder resultado = new StringBuilder();
+        HttpURLConnection conexion = null;
 
-        try{
-            URL url = new URL (urlApi);
-            HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
-           conexion.setRequestMethod("GET");
+        try {
+
+    //Convierte un String a una URL
+            URL url = new URL(urlApi);
+
+    //Configuración para la conexión HTTP
+            conexion =(HttpURLConnection) url.openConnection();
+            conexion.setRequestMethod("GET");
             conexion.setConnectTimeout(5000);
-            conexion.setReadTimeout(500);
+            conexion.setReadTimeout(5000);;
             conexion.connect();
-            
 
-        }catch(Exception e){
-        System.out.println("Error: " +e);
+    // Verificar respuesta del servidor
+            int code = conexion.getResponseCode();
+            System.out.println(code);
+
+            try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(conexion.getInputStream(),
+                StandardCharsets.UTF_8))
+                ) {
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    resultado.append(linea).append('\n');
+                }
+                }
+
+        }catch (Exception e){
+
+            System.out.println("Error: " + e);
+        }finally {
+            if (conexion != null) conexion.disconnect(); 
         }
-       
-       
-       
-       
+
         return resultado.toString();
-    }
+   }
 }
